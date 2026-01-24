dd8ad153064069a10f368ac977dfcd7106bb1a0a
Papillon Sheoron <2021eeb1175@iitrpr.ac.in>
2026-01-24 19:09:32 +0530

    dhruv ki shaadi

--- vite.config.ts
+++ vite.config.ts
@@ -61,6 +61,10 @@ export default defineConfig(({ mode }) => ({
         target: 'http://localhost:5000',
         changeOrigin: true,
       },
+      '/dhruv': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
       '/static': {
         target: 'http://localhost:5000',
         changeOrigin: true,
